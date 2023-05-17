5a6d44631491f5002e64951c52b7c930c01d9d69
alumno9 <19041239@ITDURANGO.EDU.MX>
2023-05-17 14:01:22 -0600

    Interfaz grafica agregada

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
-
-
-
-
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:techeablemachine/pages/classifier.dart';
+import 'package:image/image.dart' as img;
+import 'package:techeablemachine/pages/classifier_quant.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:logger/logger.dart';
+import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
 
-void main (){
+void main() {
   runApp(MyApp());
 }
 
@@ -13,20 +15,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-      return MaterialApp(
-        title: "Image Classification",
-        theme: ThemeData(
+    return MaterialApp(
+      title: "Image Classification",
+      theme: ThemeData(
           primaryColor: Colors.amber
-        ),
-        home: MyHomePage(
-          title:'Flutter Demo Home Page'
-        ),
-      );
+      ),
+      home: MyHomePage(
+          title: 'Flutter Demo Home Page'
+      ),
+    );
   }
 }
-class MyHomePage extends StatefulWidget{
 
-  MyHomePage({Key? key, this.title}): super(key: key);
+class MyHomePage extends StatefulWidget {
+
+  MyHomePage({Key? key, this.title}) : super(key: key);
   final String? title;
 
   @override
@@ -35,13 +38,88 @@ class MyHomePage extends StatefulWidget{
   }
 }
 
-class _MyHomePageState extends State<MyHomePage>{
+class _MyHomePageState extends State<MyHomePage> {
+
   late Classifier _classifier;
+  var logger = Logger();
+  File? _image;
+  final picker = ImagePicker();
+  Image? _imageWidget;
+  img.Image? fox;
+  Category? category;
 
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  void initState() {
+    super.initState();
+    _classifier = ClassifierQuant();
   }
+
+  Future getImage() async {
+    final pickerFile = await picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = File(pickerFile!.path);
+      _imageWidget = Image.file(_image!);
+      _predict();
+    });
+  }
+
+  void _predict() async {
+    img.Image? imageInput = img.decodeImage(_image!.readAsBytesSync());
+    var pred = _classifier.predict(imageInput!);
+
+    setState(() {
+      this.category = pred;
+    });
+
+
+
+
 }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TFlite de Perros y Gatos'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: _image == null ? Text('Ninguna imagen a sido seleccionada')
+                : Container(
+              constraints: BoxConstraints(
+                  maxHeight: MediaQuery
+                      .of(context)
+                      .size
+                      .height / 2
+              ),
+              decoration: BoxDecoration(
+                  border: Border.all()
+              ),
+              child: _imageWidget,
+            ),
+          ),
+          SizedBox(
+            height: 36,
+          ),
+          Text(
+              category != null ? category!.label:''
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+              category!=null
+                  ?'Nivel de confianza:${(category!.score).toStringAsFixed(3)}':''
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Selecciona imagen',
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+  }
